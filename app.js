12cdfb290dcc4b647df1839f094ce395041958aa
niraj1234 <niraj.webtech@gmail.com>
2025-01-16 00:14:57 +0530

    0:25:15 | Basic Server creation Reading data from file Writing data to file

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 //console.log('Hello Niraj');
-
 const http = require('http');
+const fs = require('fs');
+
+// Read data from file 
+fs.readFile('example.txt' , 'utf-8' , (err,data) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
+
+// Write content to file 
+const content = 'Hello Niraj Data to be written here';
+fs.writeFile('output.txt' , content , (err) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log("File written successfully !!!");
+})
+
+
+// Basic server creation
 const server  = http.createServer((req,res) => {
     res.statusCode = 200 ;
     res.setHeader('Content-Type' , 'text/plain');
@@ -10,3 +32,5 @@ const server  = http.createServer((req,res) => {
 server.listen( 3000 , () => {
     console.log('Server running at port 3000');
 })
+
+
