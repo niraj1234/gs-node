f24ed5bf072cd59d5ddc52c801820bcc025831af
niraj1234 <niraj.webtech@gmail.com>
2025-01-17 02:07:49 +0530

    2:03:35 | Query parameter API created and custom header implemented

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const url = require('url');
+
 
 const server = http.createServer((req,res) => {
     if(req.method === 'GET' && req.url === '/'){
@@ -13,6 +15,10 @@ const server = http.createServer((req,res) => {
             res.writeHead(200 , {'content-type':'application/json'})
             res.end(JSON.stringify({message:'Data received' , data:body}))
         })
+    } else if (req.method === 'GET'  && req.url.startsWith('/search')){
+        const queryObject = url.parse(req.url , true).query;
+        res.writeHead(200 , {"content-type":'application/json' , "cutom-header":'NodeJS Server' , "track-id":'node77654352'});
+        res.end(JSON.stringify({message:'Query received' , queryObject:queryObject}))
     } else{
         res.writeHead(404 , {'content-type': 'text/plain'});
         res.end('Requested page not found');
